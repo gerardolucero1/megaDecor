3855b3cf31533c0e12313aa4d89cb58975a2d93b
Eduardo <Eduardo@partnergrammer.com>
2019-07-29 11:38:54 -0600

    iniciando rama eduardo

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -15,7 +15,7 @@
                             We’ve put everything together, so you can start working on your Laravel project as soon as possible! Codebase assets are integrated and work seamlessly with Laravel Mix, so you can use the npm scripts as you would in any other Laravel project.
                         </p>
                         <p class="text-muted">
-                            Feel free to use any examples you like from the full versions to build your own pages. <strong>Wish you all the best and happy coding!</strong>
+                           Hola mundo
                         </p>
                     </div>
                 </div>
