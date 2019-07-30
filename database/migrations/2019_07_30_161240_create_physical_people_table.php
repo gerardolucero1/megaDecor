1a346d66e6ab0cd38dbb69f2f72e488a033eb834
Gerardo Lucero <gera_conecta@hotmail.com>
2019-07-30 12:08:20 -0600

    Se genero componente nuevo cliente

---
+++ database/migrations/2019_07_30_161240_create_physical_people_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePhysicalPeopleTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('physical_people', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nombre');
+            $table->string('apellidoPaterno');
+            $table->string('apellidoMaterno');
+            $table->string('email');
+
+            //Datos de Facturacion
+
+            $table->string('nombreFacturacion');
+            $table->string('direccionFacturacion');
+            $table->string('coloniaFacturacion');
+            $table->string('numeroFacturacion');
+            $table->string('rfcFacturacion');
+            $table->string('emailFacturacion');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('physical_people');
+    }
+}
--- resources/assets/js/laravel/components/NuevoClienteComponent.vue
+++ resources/assets/js/laravel/components/NuevoClienteComponent.vue
@@ -1,85 +1,117 @@
+<style>
+    .registroCliente .row{
+        margin-bottom: 20px;
+    }
+
+    .registroCliente input[type="text"], 
+    .registroCliente input[type="email"], 
+    .registroCliente input[type="number"], 
+    .registroCliente select{
+        width: 100%;
+    }
+</style>
+
+
 <template>
     <div class="container">
         <div class="row">
-            <div class="col-md-3 float-left">
-                <div class="">
-                    <input type="radio" name="moral" id="fisica">
-                    <label for="fisica">
-                        Persona Fisica
-                    </label>
+            <form action="POST" v-on:submit="crearCliente()">
+                <div class="col-md-10 offset-md-1 registroCliente">
+                    <div class="row">
+                        <div class="col-md-6 d-flex">
+                            <input type="radio" name="personaFisica" value="fisica" id="personaFisica">
+                            <label for="personaFisica">Persona Fisica</label>
+                        </div>
+                        <div class="col-md-6 d-flex">
+                            <input type="radio" name="personaMoral" value="moral" id="personaMoral">
+                            <label for="personaMoral">Persona Moral</label>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-4">
+                            <input type="text" placeholder="Nombres">
+                        </div>
+                        <div class="col-md-4">
+                            <input type="text" placeholder="Apellido Paterno">
+                        </div>
+                        <div class="col-md-4">
+                            <input type="text" placeholder="Apellido Materno">
+                        </div>
+                        <div class="col-md-4 mt-4">
+                            <input type="email" placeholder="Email">
+                        </div>
+                    </div>
+
+                    <h4>Telefonos de contacto</h4>
+
+                    <div class="row">
+                        <div class="col-md-4">
+                            <select name="telefonoTipo" id="">
+                                <option value="celular">Celular</option>
+                                <option value="casa">Casa</option>
+                            </select>
+                        </div>
+                        <div class="col-md-4">
+                            <input type="number" placeholder="5555555555">
+                        </div>
+                        <div class="col-md-4">
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <input type="text" placeholder="EXT">
+                                </div>
+                                <div class="col-md-6">
+                                    <button class="btn btn-sm btn-primary">Agregar</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <h4>Datos de facturacion</h4>
+
+                    <div class="row">
+                        <div class="col-md-12">
+                            <input type="text" placeholder="Nombre">
+                        </div>
+
+                        <div class="col-md-4 mt-4">
+                            <input type="text" placeholder="Direccion">
+                        </div>
+                        <div class="col-md-4 mt-4">
+                            <input type="text" placeholder="Colonia">
+                        </div>
+                        <div class="col-md-4 mt-4">
+                            <input type="text" placeholder="Numero">
+                        </div>
+                        <div class="col-md-7 mt-4">
+                            <input type="text" name="" id="" placeholder="RFC">
+                        </div>
+                        <div class="col-md-7 mt-4">
+                            <input type="email" name="" id="" placeholder="Email">
+                        </div>
+                        <div class="col-md-5 mt-4">
+                            <button class="btn btn-success btn-sm btn-block">Agregar Contacto</button>
+                        </div>
+                    </div>
+
+                    <h4>¿Como supo de nosotros?</h4>
+
+                    <div class="row">
+                        <div class="col-md-6">
+                            <select name="comoSupo" id="">
+                                <option value="recomendacion">Recomendacion</option>
+                            </select>
+                        </div>
+                    </div>
                 </div>
-            </div>
-            <div class="col-md-3 float-left"> 
-                <div class="ml-5">
-                    <input type="radio" name="moral" id="moral">
-                    <label for="moral">
-                        Persona Moral
-                    </label>
-                </div>
-            </div>
-        </div>
-        <div class="row mt-5">
-            <div class="col-md-4">
-                <input type="text" width="100%">
-            </div>
-            <div class="col-md-4">
-                <input type="text" width="100%">
-            </div>
-            <div class="col-md-4">
-                <input type="text" width="100%">
-            </div>
-            <div class="col-md-4">
-                <input type="text" width="100%">
-            </div>
-        </div>
-        <h4>Telefonos de Contacto</h4>
-        <div class="row mt-5">
-            <div class="col-md-3">
-                <input type="text">
-            </div>
-            <div class="col-md-3">
-                <input type="text">
-            </div>
-            <div class="col-md-6">
-                <input type="text">
-                <button class="btn btn-sm btn-success">Agregar</button>
-            </div>
-        </div>
-        <h4>Datos de facturacion</h4>
-        <div class="row">
-            <div class="col-md-12">
-                <input type="text">
-            </div>
-            <div class="col-md-3">
-                <input type="text">
-            </div>
-            <div class="col-md-3">
-                <input type="text">
-            </div>
-            <div class="col-md-3">
-                <input type="text">
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-md-7">
-                <input type="text">
-            </div>
-            <div class="col-md-7">
-                <input type="text">
-            </div>
-            <div class="col-md-3">
-                <button class="btn btn-success">
-                    Guardar
-                </button> 
-            </div>
+            </form>
         </div>
     </div>
 </template>
 
 <script>
     export default {
-        mounted() {
-            console.log('Component mounted.')
-        }
+        data:{
+            
+        },
     }
 </script>
