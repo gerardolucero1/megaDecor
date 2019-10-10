c3621704e241f27874cdeb1c9a8380b8ef5afb4d
Eduardo <Eduardo@partnergrammer.com>
2019-10-09 23:45:06 -0600

    cambios cambios

--- resources/views/groups/partial/form.blade.php
+++ resources/views/groups/partial/form.blade.php
@@ -20,6 +20,8 @@
             </div>
         </div>
         <div class="col-md-6">
+        </div>
+        <div class="col-md-6">
             <div class="form-group row">
                 <div class="col-md-12">
                     <div class="form-material">
