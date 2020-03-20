e4e843d39aeea03932f2027c0327f8f44847b1b7
EduardoPartnergrammer <53445429+EduardoPartnergrammer@users.noreply.github.com>
2020-03-20 10:41:12 -0700

    agregar opcion a menu

--- public/js/codebase.app.js
+++ public/js/codebase.app.js
@@ -20775,7 +20775,7 @@ function () {
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! /Users/samueleduardoacosta/Documents/GitHub/megaDecor/resources/assets/js/codebase/app.js */"./resources/assets/js/codebase/app.js");
+module.exports = __webpack_require__(/*! C:\Users\52614\Documents\GitHub\megaDecor\resources\assets\js\codebase\app.js */"./resources/assets/js/codebase/app.js");
 
 
 /***/ })
