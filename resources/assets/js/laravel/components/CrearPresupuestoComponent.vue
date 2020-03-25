aba594af9951e9d337e822accf05a6735db9a2dd
EduardoPartnergrammer <53445429+EduardoPartnergrammer@users.noreply.github.com>
2020-03-25 15:50:01 -0700

    borde modal bocadillos

--- resources/assets/js/laravel/components/CrearPresupuestoComponent.vue
+++ resources/assets/js/laravel/components/CrearPresupuestoComponent.vue
@@ -724,8 +724,8 @@ padding: 0;
         </div>
 
 <div  class="modal fade" id="bocadillosModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div style="border:solid; border-color:gray" class="modal-dialog modal-xl modal-dialog-centered" role="document">
-                <div class="modal-content">
+            <div  class="modal-dialog modal-xl modal-dialog-centered" role="document">
+                <div style="border:solid; border-color:gray" class="modal-content">
                     <div class="modal-header">
                         <h5 class="modal-title" id="exampleModalLabel">Mesas de bocadillos</h5>
                         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
