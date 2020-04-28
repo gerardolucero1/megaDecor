33c3ac07674217d911647d0bce03f30cfc97316d
EduardoPartnergrammer <53445429+EduardoPartnergrammer@users.noreply.github.com>
2020-04-28 16:28:18 -0600

    Update mesa_bocadillos.blade.php

--- resources/views/pdf/mesa_bocadillos.blade.php
+++ resources/views/pdf/mesa_bocadillos.blade.php
@@ -17,7 +17,7 @@
                             </td>
                         <td style="text-align: right">
                                 <span style="font-weight: bold; font-size: 14px">Folio de contrato:</span> <span>1000 </span><br>
-                            <span  style="font-style: italic; font-size: 14px; font-weight: bold">Fecha del evento: </span><span><span>
+                                <span  style="font-style: italic; font-size: 14px; font-weight: bold">Fecha del evento: </span>Martes 28 Abril 2020<span><span>
 
                             </td>
                         </tr>
@@ -28,22 +28,47 @@
                         <div style="width: 100%; height: 0px"></div>
                         
 
-                        <label for="" style="font-size: 10px; font-style: italic">Abono actual</label>
+                        <label for="" style="font-size: 16px;">Numero de personas: 100</label>
+                        <label for="" style="font-size: 16px;">Numero de Bocadillos: 400</label>
+                        <label for="" style="font-size: 16px;">Numero de servilletas: 100</label>
+                        <label for="" style="font-size: 16px;">Numero de platos: 100</label>
                         <table style="width: 100%; font-size: 13px"> 
                             <tr>
+                                <td style="padding:4px; background:#E8E8E8; text-align: center">Imagen</td>
                                 <td style="padding:4px; background:#E8E8E8; text-align: center">Concepto</td>
-                                <td style="padding:4px; background:#E8E8E8; text-align: center">Fecha y hora de pago</td>
-                                <td style="padding:4px; background:#E8E8E8; text-align: center">Metodo de pago</td>
-                                <td style="padding:4px; background:#E8E8E8; text-align: center">Monto</td>
+                                <td style="padding:4px; background:#E8E8E8; text-align: center">Bocadillos por paquete</td>
+                                <td style="padding:4px; background:#E8E8E8; text-align: center">Paquetes</td>
+                                <td style="padding:4px; background:#E8E8E8; text-align: center">Total</td>
                             </tr>
                             <tr style="text-align: center">
-                                <td>Abono a contrato</td>
-                                <td></td>
-                                <td></td>
+                                <td><img style="width:80px;" src="http://megamundo.com.mx/Eventos/Admin/Images/Servicios/131105214.jpg"></td>
+                                <td>ROLLES DE CANELA GLASEADOS O NATURALES</td>
+                                <td>5</td>
+                                <td>10</td>
+                                <td>50</td>
+                            </tr>
+                            <tr style="text-align: center">
+                                <td><img style="width:80px;" src="http://megamundo.com.mx/Eventos/Admin/Images/Servicios/273185115.jpg"></td>
+                                <td>EMPANADAS CON  PEPPERONI  Y QUESO 30 PZ ($12 C/U)</td>
+                                <td>2</td>
+                                <td>10</td>
+                                <td>20</td>
+                            </tr>
+                            <tr style="text-align: center">
+                                <td><img style="width:80px;" src="http://megamundo.com.mx/Eventos/Admin/Images/Servicios/257203017.jpg"></td>
+                                <td>	CHAROLA REDONDA CON WRAPS 45pz (minimo 3 charolas)</td>
+                                <td>10</td>
+                                <td>30</td>
+                                <td>300</td>
+                            </tr>
+                            <tr>
                             <td></td>
+                            <td style="background:red; color:white">370</td>
                             </tr>
                         </table>
-                        <div style="width: 100%; height: 0px; border-bottom:solid"></div>
+                        <div style="width: 100%; height: 0px; border-bottom:solid">Subtotal: $7,820.00</div>
+                        <div style="width: 100%; height: 0px; border-bottom:solid">Descuento: $320.00</div>
+                        <div style="width: 100%; height: 0px; border-bottom:solid">Total: $7,500.00</div>
                         
                       
                 </div>
