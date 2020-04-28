ed0ed3b96b573dd4a756c28852119e5fb9735d3d
EduardoPartnergrammer <53445429+EduardoPartnergrammer@users.noreply.github.com>
2020-04-28 10:26:57 -0600

    123

--- routes/api.php
+++ routes/api.php
@@ -37,6 +37,10 @@ Route::get('inventariott', function(){
 
 Route::get('clientestt', function(){
    $inventory = App\Inventory::orderBy('id', 'DESC')->Where('archivar', null)->orWhere('archivar', false)->get();
+   $clientes_fisicos = DB::table('clients')
+        ->join('physical_people', 'physical_people.client_id', '=', 'clients.id')
+        ->select( 'clients.id', 'physical_people.nombre', 'physical_people.apellidoPaterno', 'physical_people.apellidoMaterno', 'physical_people.email', 'physical_people.nombreFacturacion', 'physical_people.direccionFacturacion', 'physical_people.coloniaFacturacion', 'physical_people.numeroFacturacion', 'physical_people.created_at')
+        ->get();
        // $inventarioBudget = BudgetInventory::where('guardarInventario', true)->get()->toArray();
      //   $inventarioPack = BudgetPackInventory::where('guardarInventario', true)->get()->toArray();
         
@@ -44,10 +48,10 @@ Route::get('clientestt', function(){
         //dd($inventarioAuth);
 
     return datatables()
-    ->of($inventory)
+    ->of($clientes_fisicos)
     // ->eloquent(App\Inventory::query())
-    ->addColumn('btn', 'actions')
-    ->addColumn('img', 'images')
-    ->rawColumns(['btn', 'img'])
+    //->addColumn('btn', 'actions')
+    //->addColumn('img', 'images')
+    //->rawColumns(['btn', 'img'])
     ->toJson();
 });
