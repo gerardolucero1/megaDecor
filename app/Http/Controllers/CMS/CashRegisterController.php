63d89935e31c022c0b074f7b21a4bee3341a969b
Eduardo <Eduardo@partnergrammer.com>
2020-07-09 23:30:30 -0600

    Update CashRegisterController.php

--- app/Http/Controllers/CMS/CashRegisterController.php
+++ app/Http/Controllers/CMS/CashRegisterController.php
@@ -180,13 +180,15 @@ class CashRegisterController extends Controller
 
         $date = Carbon::now();
         $fechaCorte = $registro->created_at;
+        $fechaCorteCierre = $registro->updated_at;
         $date=$date->format('Y-m-d');
         $fechaCorte = $fechaCorte->format('Y-m-d');
+        $fechaCorteCierre = $fechaCorteCierre->format('Y-m-d');
 
 
         $fechaApertura = Carbon::parse($registro->created_at);
         $fechaCierre = Carbon::parse($registro->updated_at);
-        $pagos = Payment::with('budget')->orderBy('id', 'DESC')->whereDate('created_at', $fechaCorte)->whereTime('created_at', '>=', $registro->horaApertura)->whereTime('created_at', '<=', $registro->horaCierre)->get();
+        $pagos = Payment::with('budget')->orderBy('id', 'DESC')->whereDate('created_at', '>=', $fechaCorte)->whereTime('created_at', '>=', $registro->horaApertura)->whereDate('created_at', '>=', $fechaCorteCierre)->whereTime('created_at', '<=', $registro->horaCierre)->get();
         $otrosPagos = OtherPayments::orderBy('id', 'DESC')->whereDate('created_at', $fechaCorte)->whereTime('created_at', '>=', $registro->horaApertura)->whereTime('created_at', '<=', $registro->horaCierre)->get();
         
 
