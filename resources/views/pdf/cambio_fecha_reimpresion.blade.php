ceefe533194f4ea00e3776dbccda227205940ccd
EduardoPartnergrammer <53445429+EduardoPartnergrammer@users.noreply.github.com>
2020-10-07 03:31:33 -0600

    Update cambio_fecha_reimpresion.blade.php

--- resources/views/pdf/cambio_fecha_reimpresion.blade.php
+++ resources/views/pdf/cambio_fecha_reimpresion.blade.php
@@ -14,6 +14,8 @@
         <div style="width: 100%; margin-top;-20px">
             <p style="font-size: 100px; position:absolute; font-weight:bold; color:rgba(238,37,37,.2); text-align:center; transform: rotate(-45deg); margin-left:300px; margin-top:100px;">Reimpresión</p>
                 <p style="text-align:center; font-weight:bold"><span style="font-style: italic; font-size:16px">Folio de contrato:</span> <span> {{$budget->folio}}</span></p>
+               
+               
                 <table style="width: 100%; font-family: Helvetica;" >
                         <tr>
                             <td>
@@ -40,14 +42,28 @@
                             </td>
                         </tr>
 
-                    <p>Cmbios Previos Solicitados: {{count($totalCambios)}}</p>
-                       
-                    <p style="font-weight: bold; padding:20px">Motivo de modificación en fecha: <br><span style="font-style: italic; font-weight:lighter">{{$ultimoCambio->motivo}}</span></p>
-                    
-                
-
                         </table>
-                   
+
+                        <p style="font-size: 100px; position:absolute; font-weight:bold; color:rgba(238,37,37,.2); text-align:center; transform: rotate(-45deg); margin-left:300px; margin-top:100px;">Reimpresión</p>
+
+                        <p style="font-weight: bold; padding:20px"> <span>Cmbios Previos Solicitados: {{count($totalCambios)}}</span><br>Motivo de modificación en fecha: <br><span style="font-style: italic; font-weight:lighter">{{$ultimoCambio->motivo}}</span></p>
+                        
+                        <table style="width: 100%">
+                            <tr>
+                            <th>Fecha anterior</th>
+                            <th>Fecha despues del cambio</th>
+                            <th>Vigencia</th>
+                            </tr>
+                            @foreach ($totalCambios as $cambios)
+                                <tr>
+                                <td>{{$cambios->fechaAnterior}}</td>
+                                <td>@if($cambios->fechaNueva==null) Pendiente @else{{$cambios->fechaNueva}} @endif</td>
+                                <td>{{$cambios->vigencia}}</td>
+                                </tr>
+                            @endforeach
+                        </table>
+
+
                                 <table style="width: 100%; margin-top: -10px">
                                     <tr>
                                     <td><p style="text-align: center">______________________________<br>Firma Ivonne C. Arroyos Piñon</p></td>
