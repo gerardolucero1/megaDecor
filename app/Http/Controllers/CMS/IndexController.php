050ce49d395a7799079075915aaccdd4722d0e3f
EduardoPartnergrammer <53445429+EduardoPartnergrammer@users.noreply.github.com>
2020-10-12 03:32:46 -0600

    endientes

--- app/Http/Controllers/CMS/IndexController.php
+++ app/Http/Controllers/CMS/IndexController.php
@@ -1509,7 +1509,7 @@ public function archivarUsuario($id){
 
          //calculo adeudo total
          $adeudoTotal = 0;
-         $contratosAdeudo = Budget::orderBy('id', 'DESC')->where('pagado', null)->where('archivado', 'FALSE')->where('tipo', 'CONTRATO')->where('fechaEvento', '!=', null)->get();
+         $contratosAdeudo = Budget::orderBy('id', 'DESC')->where('pagado', '!=', true)->where('archivado', 'FALSE')->where('tipo', 'CONTRATO')->where('fechaEvento', '!=', null)->get();
          
           //Fincalculo adeudo total
 
