c1f395024f4226b0f9bbf4856915ed14279e6057
EduardoPartnergrammer <eduardo@partnergrammer.com>
2020-10-20 08:27:34 -0600

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -305,7 +305,7 @@ Route::group(['middleware' => ['auth']], function () {
             }
          }
     
-        Mail::to('asamii149@gmail.com', 'Administrador')
+        Mail::to('ivonnearroyosdecor@hotmail.com', 'Administrador')
             ->send(new NuevoPresupuesto($presupuesto, $Telefonos, $Elementos));
     });
 
@@ -418,7 +418,7 @@ Route::group(['middleware' => ['auth']], function () {
     
         Mail::to($email, 'Presupuesto MegaMundo')
             ->send(new NuevoPresupuesto($presupuesto, $Telefonos, $Elementos, $Paquetes, $arregloEmentos));
-        Mail::to('ivonnearroyosg@msn.com', 'Presupuesto MegaMundo')
+        Mail::to('ivonnearroyosdecor@hotmail.com', 'Presupuesto MegaMundo')
             ->send(new NuevoPresupuesto($presupuesto, $Telefonos, $Elementos, $Paquetes, $arregloEmentos));
     });
 
@@ -569,7 +569,7 @@ Route::group(['middleware' => ['auth']], function () {
         $otrosPagos = OtherPayments::orderBy('id', 'DESC')->whereDate('created_at', $fechaCorte)->whereTime('created_at', '>=', $registro->horaApertura)->whereTime('created_at', '<=', $registro->horaCierre)->get();
         
 
-        Mail::to('ivonnearroyosg@msn.com', 'Corte de caja')
+        Mail::to('ivonnearroyosdecor@hotmail.com', 'Corte de caja')
             ->send(new CorteCaja($registro, $pagos, $otrosPagos));
     });
 
