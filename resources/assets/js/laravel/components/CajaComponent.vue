0e9093a3e201a0b634fec63465a13ffc66142418
EduardoPartnergrammer <eduardo@partnergrammer.com>
2020-10-27 00:00:05 -0700

    as

--- resources/assets/js/laravel/components/CajaComponent.vue
+++ resources/assets/js/laravel/components/CajaComponent.vue
@@ -1224,7 +1224,7 @@ if(element.tipo == 'INGRESO'){
                 let dolar = this.sesion.cantidadDolares;
                 let transferencias = 0;
                 let Ptarjeta = 0;
-                alert(this.sesion.cantidadDolares);
+                alert(this.sesionActual.cantidadDolares);
 
 
                 this.pagosCorte[0].forEach((element) => {
