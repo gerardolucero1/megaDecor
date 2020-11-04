dcd57302b1d68ac5dc76a49112bb0171619a91de
EduardoPartnergrammer <eduardo@partnergrammer.com>
2020-11-04 00:28:11 -0700

    Update IndexController.php

--- app/Http/Controllers/CMS/IndexController.php
+++ app/Http/Controllers/CMS/IndexController.php
@@ -530,6 +530,7 @@ public function archivarUsuario($id){
         //calculo adeudo total
         $adeudoTotal = 0;
         $contratosAdeudo = Budget::orderBy('id', 'DESC')->where('pagado', '!=', true)->where('archivado', 'FALSE')->where('tipo', 'CONTRATO')->where('fechaEvento', '!=', null)->get();
+        dd($contratosAdeudo);
         foreach($contratosAdeudo as $contratoAdeudo){
             $totalAbono=0;
             $pagosContrato = Payment::where('budget_id', $contratoAdeudo->id)->get();
