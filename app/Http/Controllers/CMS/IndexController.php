e73a624d587610b51b2e47f5c5b2673a91cb6a47
eduardoavance <74919327+eduardoavance@users.noreply.github.com>
2021-01-05 09:26:56 -0700

    Update IndexController.php

--- app/Http/Controllers/CMS/IndexController.php
+++ app/Http/Controllers/CMS/IndexController.php
@@ -532,7 +532,7 @@ public function archivarUsuario($id){
         
         //calculo adeudo total
         $adeudoTotal = 0;
-        $contratosAdeudo = Budget::orderBy('id', 'DESC')->where('archivado', 'FALSE')->where('tipo', 'CONTRATO')->where('fechaEvento', '!=', null)->get();
+        $contratosAdeudo = Budget::orderBy('id', 'DESC')->where('pagado', '!=', true)->where('archivado', 'FALSE')->where('tipo', 'CONTRATO')->where('fechaEvento', '!=', null)->get();
         dd($contratosAdeudo);
 
 
