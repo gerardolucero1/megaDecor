a4bef4f82e8068abef686b9bc5b8c74cc1a0582f
eduardoavance <74919327+eduardoavance@users.noreply.github.com>
2021-01-05 14:32:34 -0700

    Update creditosAtrasados.blade.php

--- resources/views/creditosAtrasados.blade.php
+++ resources/views/creditosAtrasados.blade.php
@@ -46,7 +46,7 @@
                             </thead>
                             <tbody>
                                 @php
-                                    $telefonoCliente = 0;
+                                    $telefonoCliente = '';
                                 @endphp
                                 @foreach ($contratos as $budgetArchivados) 
                                 @php
@@ -93,12 +93,12 @@
                                             $clienteFisico = App\PhysicalPerson::where('client_id', $budgetArchivados->client_id)->first();
                                             $clienteNombre = $clienteFisico->nombre.' '.$clienteFisico->apellidoPaterno.' '.$clienteFisico->apellidoMaterno;
                                            // $clienteCompleto = App\PhysicalPerson::where('client_id', $cliente->id)->first();
-                                           $telefonoCliente = $clienteFisico->telefono;
+                                            //$telefonoCliente = $clienteFisico->telefono;
 
                                         }else{
                                             $clienteMoral = App\MoralPerson::where('client_id', $budgetArchivados->client_id)->first();
                                             $clienteNombre = $clienteMoral->nombre;
-                                            $telefonoCliente = $clienteMoral->telefono;
+                                            //$telefonoCliente = $clienteMoral->telefono;
                                         }
                                     @endphp
                                             <td class="d-none d-sm-table-cell">{{$clienteNombre}}</td>
