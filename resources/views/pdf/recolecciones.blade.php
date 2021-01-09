2f4db95d58c2245e82b77050d3eceef42613dc75
EduardoPartnergrammer <53445429+EduardoPartnergrammer@users.noreply.github.com>
2021-01-09 15:26:09 -0700

    ghjk

--- resources/views/pdf/recolecciones.blade.php
+++ resources/views/pdf/recolecciones.blade.php
@@ -30,7 +30,9 @@
                 <th>Teléfono</th>
                 @endif
             </tr>
+            @if($tipoImpresion != 'RECOLECCION')
             @foreach ($presupuestos as $presupuesto)
+            
             <tr style="border:solid; text-align: center">
             <td style="padding: 5px;">{{$presupuesto->folio}}</td>
             @php
@@ -93,6 +95,76 @@
                 @endif
             </tr>
             @endforeach
+            @else
+            @foreach ($presupuestos as $presupuesto)
+            @if($presupuesto->entregaEnBodega)
+            @else
+            <tr style="border:solid; text-align: center">
+                <td style="padding: 5px;">{{$presupuesto->folio}}</td>
+                @php
+                     $fechaEvento = Carbon\Carbon::parse($presupuesto->fechaEvento)->locale('es');
+                @endphp
+                <td style="width: 150px">{{$fechaEvento->translatedFormat(' l j F Y')}}</td>
+                <td>{{$presupuesto->horaEventoInicio}} {{$presupuesto->inicioAmPm}}</td>
+                <td>
+    @php
+        $cliente = App\Client::where('id', $presupuesto->client_id)->first();
+        if($cliente->tipoPersona=='FISICA'){
+            $datosCliente = App\PhysicalPerson::where('client_id', $cliente->id)->first();
+            $nombreCliente = $datosCliente->nombre.' '.$datosCliente->apellidoPaterno.' '.$datosCliente->apellidoMaterno;
+        }else{
+            $nombreCliente =  $cliente->nombreCliente;
+        }
+    @endphp
+    <p style="font-size: 10px">
+    {{$nombreCliente}}</p>
+    
+                </td>
+                <td>
+                @php
+                    $vendedor = App\User::where('id', $presupuesto->vendedor_id)->first();
+                @endphp
+               <p style="font-size: 10px"> {{$vendedor->name}}</p>
+                </td>
+                <td>{{$presupuesto->version}}</td>
+                <td>
+                    @if($presupuesto->entregaEnBodega)
+                    Entrega en bodega
+                    @else
+                    Recolección
+                    @endif
+                    </td>
+                    @php
+                        $totalPagos=0;
+                            $pagos = App\Payment::where('budget_id', $presupuesto->id)->get();
+                        @endphp
+                        @foreach ($pagos as $pago)
+                            @php
+                                $totalPagos = $totalPagos+$pago->amount;
+                            @endphp
+                        @endforeach
+                        @if($tipoImpresion != 'RECOLECCION')
+                    <td >
+                        @if ($presupuesto->opcionIVA)
+                        ${{number_format(($presupuesto->total*1.16)-$totalPagos,2)}}
+                        @else
+                        ${{number_format(($presupuesto->total)-$totalPagos,2)}}
+                        @endif
+                        
+                    </td>
+                    @endif
+                    @if($tipoImpresion == 'RECOLECCION')
+                    <td >
+                       telefono
+                        
+                    </td>
+                    @endif
+
+                </tr>
+                @endif
+            @endforeach
+            @endif
+            
             
         </table>
        <div>
