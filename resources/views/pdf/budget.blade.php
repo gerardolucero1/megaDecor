abd2592a351295758b1e33e92724b9c26f28db69
eduardoavance <74919327+eduardoavance@users.noreply.github.com>
2021-04-13 13:50:32 -0600

    Update budget.blade.php

--- resources/views/pdf/budget.blade.php
+++ resources/views/pdf/budget.blade.php
@@ -144,7 +144,7 @@ Dias de credito: {{$presupuesto->diasCredito}}  <br>
   @endphp
   @foreach ($Elementos as $elemento)
   @php
-      $descuento=$descuento+(intval($elemento->ahorro)*$elemento->cantidad);
+      $descuento=$descuento+(floatval($elemento->ahorro)*floatval($elemento->cantidad));
       $c++;
       $familias[$c]=$elemento->familia;
   @endphp
