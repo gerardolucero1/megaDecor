9486a6e96005d3784a3e2d037b566d7d79f0f2da
Eduardo <Eduardo@partnergrammer.com>
2021-06-23 20:42:20 -0600

    Revert "Update InventoryController.php"
    This reverts commit adb39a4745b7ebc8976a9f4eb15faa3d9fa9bda4.

--- app/Http/Controllers/CMS/InventoryController.php
+++ app/Http/Controllers/CMS/InventoryController.php
@@ -51,8 +51,7 @@ class InventoryController extends Controller
 
     public function insertarProducto(Request $request)
     {
-        dd($request);
-         //C$Romprobamos que el slug no se repita pero ignoramos el slug propio
+         //Comprobamos que el slug no se repita pero ignoramos el slug propio
          $v = \Validator::make($request->all(), [
             'cantidad' => 'required',
             'servicio' => 'required',
