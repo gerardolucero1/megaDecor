f8dffc8d7a386f674dc66401bdab1dbbb8d88f4c
Eduardo <Eduardo@partnergrammer.com>
2021-06-23 19:04:29 -0600

    Update InventoryController.php

--- app/Http/Controllers/CMS/InventoryController.php
+++ app/Http/Controllers/CMS/InventoryController.php
@@ -85,8 +85,7 @@ class InventoryController extends Controller
         $registro->user_id = Auth::user()->id;
         $registro->save();
 
-        return redirect()->route('inventory.create')
-            ->with('info', 'Producto creado con exito');
+        return $producto;
 
     }
 
