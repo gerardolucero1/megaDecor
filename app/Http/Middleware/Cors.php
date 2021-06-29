187a3dedd1f88ad7fff888317e68cedd39878738
eduardoavance <74919327+eduardoavance@users.noreply.github.com>
2021-06-29 12:35:09 -0600

    Update Cors.php

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -7,10 +7,11 @@ class Cors
   {
     return $next($request)
        //Url a la que se le dará acceso en las peticiones
-      ->header("Access-Control-Allow-Origin", '*')
+      ->header("Access-Control-Allow-Origin", "*")
+      ->header("Access-Control-Allow-Credentials", "false")
       //Métodos que a los que se da acceso
       ->header("Access-Control-Allow-Methods", "*")
       //Headers de la petición
-      ->header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, X-Token-Auth, Authorization"); 
+      ->header("Access-Control-Allow-Headers", "Origin, Content-Type, X-Auth-Token , Authorization"); 
   }
 }
